4311d250e814c6e6771d336669126b871a4d81d3
Nafiur Rashid <nafiurrashid@gmail.com>
2022-06-16 17:05:12 +0600

    Changed to ModelSerializer created

--- api_basic/serializers.py
+++ api_basic/serializers.py
@@ -1,3 +1,4 @@
+from dataclasses import fields
 from rest_framework import serializers
 from .models import Article
 
@@ -8,12 +9,6 @@ class ArticleSerializer(serializers.Serializer):
     date =serializers.DateField()
 
     def create(self, validated_data):
-        return Article.objects.create(validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title= validated_data.get('title', instance.title)
-        instance.author= validated_data.get('author', instance.author)
-        instance.email= validated_data.get('email', instance.email)
-        instance.date= validated_data.get('date', instance.date)
-        instance.save()
-        return instance
+        class Meta:
+            model = Article
+            fields=['id', 'title', 'author']
