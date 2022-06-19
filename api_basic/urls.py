ea91a3dcdda511373e2cc1ca9dcd4b47c033d223
Nafiur Rashid <nafiurrashid@gmail.com>
2022-06-19 22:39:20 +0600

    Half function based half class based

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import article_list, article_details
+from .views import article_details,ArticleAPIView
 
 urlpatterns = [
-    path('article/', article_list),
+    # path('article/', article_list),
+    path('article/', ArticleAPIView.as_view()),
     path ('detail/<int:pk>/', article_details)
 ]
--- api_basic/views.py
+++ api_basic/views.py
@@ -3,25 +3,42 @@ from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
 from .models import Article
 from .serializers import ArticleSerializer
-from django.views.decorators.csrf import csrf_exempt
+# from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 # Create your views here.
-@api_view(['GET','POST'])
-def article_list(request):
-    if request.method=='GET':
+class ArticleAPIView(APIView):
+    def get(self,request):
         articles= Article.objects.all()
         serializer= ArticleSerializer(articles,many=True)
         return Response(serializer.data)
-
-    elif request.method == 'POST':
+    
+    def post(self, request):
         serializer=ArticleSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+# @api_view(['GET','POST'])
+# def article_list(request):
+#     if request.method=='GET':
+#         articles= Article.objects.all()
+#         serializer= ArticleSerializer(articles,many=True)
+#         return Response(serializer.data)
+
+#     elif request.method == 'POST':
+#         serializer=ArticleSerializer(data=request.data)
+
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def article_details(request,pk):
