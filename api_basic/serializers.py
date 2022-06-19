853ee74899cbc505ac8d0a48f109104f577ed73f
Nafiur Rashid <nafiurrashid@gmail.com>
2022-06-19 20:01:56 +0600

    Function-Based view: list view with get-post , detail view with CRUD

--- api_basic/serializers.py
+++ api_basic/serializers.py
@@ -1,14 +1,8 @@
-from dataclasses import fields
 from rest_framework import serializers
 from .models import Article
 
-class ArticleSerializer(serializers.Serializer):
-    title =serializers.CharField(max_length=100)
-    author =serializers.CharField(max_length=100)
-    email =serializers.EmailField(max_length=100)
-    date =serializers.DateField()
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields=['id', 'title', 'author']
 
-    def create(self, validated_data):
-        class Meta:
-            model = Article
-            fields=['id', 'title', 'author']
