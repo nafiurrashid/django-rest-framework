fe3292e70cc2a594522b83c62409053e5ff394d8
Nafiur Rashid <nafiurrashid@gmail.com>
2022-06-29 05:29:40 +0600

    feat: generic api view completed for single api

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import article_details,ArticleAPIView, ArticleDetails
+from .views import article_details,ArticleAPIView, ArticleDetails, GenericAPIView
 
 urlpatterns = [
     # path('article/', article_list),
     path('article/', ArticleAPIView.as_view()),
+    # GenericAPIView
+     path('generic/article/<int:id>/', GenericAPIView.as_view()),
     path('detail/<int:id>/',ArticleDetails.as_view())
     # path ('detail/<int:pk>/', article_details)
 ]
